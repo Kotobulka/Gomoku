327f4dc276328e422e02bdd6d17b08c3a944ecbc
Kotobulka <144238123+Kotobulka@users.noreply.github.com>
2024-04-23 16:30:54 +0300

    Game_board_prototipe.py

--- Game_board_prototipe.py
+++ Game_board_prototipe.py
@@ -9,33 +9,22 @@ class MemoryGame(QWidget):
         super().__init__()
 
         self.setWindowTitle('Memory Game')
-        self.setGeometry(100, 100, 400, 600)
-
+        self.setGeometry(100, 100, 600, 600)
+        self.setFixedSize(1000, 1000)
         self.timer = QTimer()
         self.timer.timeout.connect(self.updateTimer)
-
         self.timeLabel = QLabel()
         self.timerValue = QTime(0, 10, 0)  # Инициализируем таймер на 10 минут
         self.updateTimerLabel()
-
         self.board = QGridLayout()
-        self.initializeBoard()
-
         layout = QVBoxLayout()
         layout.addLayout(self.board)
         layout.addWidget(self.timeLabel)
-
         game_board = GameBoard()  # Создаем игровое поле
         layout.addWidget(game_board)
-
         self.setLayout(layout)
-
         self.show()
         self.timer.start(1000)  # Таймер обновляется каждую секунду
-
-    def initializeBoard(self):
-        # Создайте игровое поле с кнопками (клетками)
-        pass  # Добавьте логику инициализации игрового поля здесь
 
     def updateTimer(self):
         self.timerValue = self.timerValue.addSecs(-1)
@@ -49,32 +38,51 @@ class MemoryGame(QWidget):
     def updateTimerLabel(self):
         self.timeLabel.setText(f"Оставшееся время: {self.timerValue.toString('mm:ss')}")
 
-
 class GameBoard(QWidget):
     def __init__(self):
         super().__init__()
-
         self.initUI()
+
+    def buttonClicked(self, i, j):
+        self.buttonClickedSignal.emit(i, j)
+        if self.valuesMatrix[i][j] == 0:  # Проверяем, что клетка пустая
+            self.valuesMatrix[i][j] = self.currentPlayer
+            index = 15 * i + j
+            if self.currentPlayer == 1:
+                self.buttonMatrix[index].setStyleSheet('''QPushButton {border: none; margin: 0px; padding: 0px; border-image: url(board_white.png);}''')
+            else:
+                self.buttonMatrix[index].setStyleSheet('''QPushButton {border: none; margin: 0px; padding: 0px; border-image: url(board_black.png);}''')
+
+            if self.checkWin(i, j):
+                self.timeLabel.setText(f"Победитель: Игрок {self.currentPlayer}")  # Вывод победителя
+                self.timer.stop()  # Останавливаем таймер после победы
+                self.disableButtons()  # Деактивируем кнопки после победы
+            else:
+                self.currentPlayer *= -1  # Смена игрока, если нет победителя
+
 
     def initUI(self):
         self.setWindowTitle('Game Board')
-        self.setGeometry(0, 0, 600, 600)  # Устанавливаем размер окна 600 на 600
-
-        size = 15  # Размер игрового поля (вы можете поменять на другое значение)
+        self.setGeometry(0,0, 600, 600)  # Устанавливаем размер окна 600 на 600
 
         self.gridLayout = QGridLayout()  # Создаем сетку для размещения кнопок
         self.gridLayout.setSpacing(0)  # Устанавливаем отсутствие отступов между кнопками
 
         self.buttonMatrix = []  # Матрица для хранения кнопок
-        self.valuesMatrix = [[0 for _ in range(size)] for _ in range(size)]  # Матрица для хранения значений кнопок
+        self.valuesMatrix = [[0 for _ in range(15)] for _ in range(15)]  # Матрица для хранения значений кнопок
 
-        for i in range(size):
-            for j in range(size):
+        for i in range(15):
+            for j in range(15):
                 button = QPushButton(f'({i}, {j})', self)
-                button.setFixedSize(40, 40)  # Устанавливаем фиксированный размер кнопки, делаем ее квадратной
+                button.setFixedSize(64, 64)  # Устанавливаем фиксированный размер кнопки, делаем ее квадратной
+                button.collate = QPushButton("Collate")
+                button.setStyleSheet('''QPushButton {border: none; margin: 0px; padding: 0px; border-image: url(board.png);}''')
                 button.clicked.connect(lambda _, i=i, j=j: self.buttonClicked(i, j))  # Подключаем обработчик клика
                 self.buttonMatrix.append(button)
                 self.gridLayout.addWidget(button, i, j)
+                self.setLayout(self.gridLayout)
+                button.setText('')
+
 
         center_widget = QWidget()
         center_layout = QVBoxLayout()
@@ -91,19 +99,20 @@ class GameBoard(QWidget):
 
         self.setLayout(main_layout)
 
-        center = size // 2
-        index = size * center + center
-        self.buttonMatrix[index].setText('1')  # Устанавливаем первое значение 1 в центре
-        self.valuesMatrix[center][center] = 1
 
-        self.currentPlayer = -1  # Начинаем с -1 (второй игрок)
+
+        self.currentPlayer = 1  # Начинаем с -1 (второй игрок)
 
     def buttonClicked(self, i, j):
         if self.valuesMatrix[i][j] == 0:  # Проверяем, что клетка пустая
             self.valuesMatrix[i][j] = self.currentPlayer
             index = 15 * i + j
-            self.buttonMatrix[index].setText(str(self.currentPlayer))
-
+            if self.currentPlayer == 1:
+                self.buttonMatrix[index].setStyleSheet('''QPushButton {border: none; margin: 0px; padding: 0px; border-image: url(board_white.png);}''')
+                self.buttonMatrix[index].setText(str(self.currentPlayer))
+            else:
+                self.buttonMatrix[index].setStyleSheet('''QPushButton {border: none; margin: 0px; padding: 0px; border-image: url(board_black.png);}''')
+                self.buttonMatrix[index].setText(str(self.currentPlayer))
             self.currentPlayer *= -1  # Смена игрока
 
 
